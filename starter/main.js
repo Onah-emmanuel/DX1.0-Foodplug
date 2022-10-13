a7a2b272494772051ca608d7a91af1085d7b491c
Onah-emmanuel <onahemmanueluchenna@gmail.com>
2022-10-13 13:54:37 +0100

    ongoing hamburger section

---
+++ starter/main.js
@@ -0,0 +1,15 @@
+// ES5
+
+// console.log("send me your account number")
+// var writeup = "I am hungry"
+// alert(writeup)
+// console.log(document)
+// document.querySelector(".hamburger")
+
+
+var hamburgerdiv = document.querySelector(".hamburger")
+// console.log(hamburgerdiv)
+
+function dosomething() {
+    hamburgerdiv.classList.toggle("showburger")
+}
