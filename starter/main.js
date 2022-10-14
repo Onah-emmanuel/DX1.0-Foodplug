48222f20236bb17fee2b2db16692567427371df8
Onah-emmanuel <onahemmanueluchenna@gmail.com>
2022-10-14 12:48:49 +0100

    completed the mobile part

--- starter/main.js
+++ starter/main.js
@@ -7,9 +7,12 @@
 // document.querySelector(".hamburger")
 
 
-var hamburgerdiv = document.querySelector(".hamburger")
+var hamburgerdiv = document.querySelector(".hamburger");
 // console.log(hamburgerdiv)
-
+var mobileLinks = document.querySelector(".mobile-links-holder");
+var mybackup = document.querySelector(".backdrop")
 function dosomething() {
     hamburgerdiv.classList.toggle("showburger")
+    mobileLinks.classList.toggle("show-mobile-links-holder")
+    mybackup.classList.toggle("show-backdrop")
 }
